d958a8b92daf99e70472db9fc7365f5a738878e3
Eric Ni <ericni2000@gmail.com>
2021-08-11 16:20:12 -0700

    Fixed up search bar and started people component

---
+++ messengerclone/src/Components/PeopleCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./PeopleCard.module.css";
+
+export default function PeopleCard() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.pfp}>
+        
+      </div>
+    </div>
+  )
+}
