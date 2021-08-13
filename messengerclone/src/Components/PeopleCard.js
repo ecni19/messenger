84549d65370af0b8d5402c48f499949ffde94394
Eric Ni <ericni2000@gmail.com>
2021-08-12 22:51:01 -0700

    Added scrollbar and nearly done with peopleCard

--- messengerclone/src/Components/PeopleCard.js
+++ messengerclone/src/Components/PeopleCard.js
@@ -4,9 +4,12 @@ import styles from "./PeopleCard.module.css";
 export default function PeopleCard() {
   return (
     <div className={styles.container}>
-      <div className={styles.pfp}>
-        
-      </div>
+      <div className={styles.pfp} />
+      <ul>
+        <div className={styles.name}>Name</div>
+        <div className={styles.lastMsg}>Last Message</div>
+      </ul>
+      <div className={styles.read}/>
     </div>
-  )
+  );
 }
--- messengerclone/src/Components/SidePanel.js
+++ messengerclone/src/Components/SidePanel.js
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./SidePanel.module.css";
 import Search from "./Search";
+import People from "./People";
 
 export default function SidePanel() {
-  return(
+  return (
     <div className={styles.container}>
-      <Search/>      
+      <Search />
+      <div className={styles.peopleCont}>
+        <People />
+      </div>
     </div>
-  )
+  );
 }
 
