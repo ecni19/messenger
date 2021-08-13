cd3b77212c1b255c2b9eb2eeb62adac32671cf05
Eric Ni <ericni2000@gmail.com>
2021-08-12 22:52:28 -0700

    Repushing

---
+++ messengerclone/src/Components/People.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./People.module.css";
+import PeopleCard from "./PeopleCard";
+
+export default function People() {
+  return (
+    <div className={styles.container}>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+      <PeopleCard/>
+    </div>
+  )
+}
